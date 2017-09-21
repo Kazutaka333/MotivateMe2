dc597b2909d1c637310a72f4358889e42b60cd94
Kazutaka Homma <special18fender@gmail.com>
2017-09-21 19:54:28 +0900

    Initial Commit

---
+++ Motivate Me 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Motivate Me 2
+//
+//  Created by Kazutaka Homma on 9/21/17.
+//  Copyright © 2017 Kazutaka Homma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
